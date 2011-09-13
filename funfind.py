3fd8592951eaa5c36ee26a3caabc157aedf7f376
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2011-09-13 18:16:59 +0200

    rewire error accumulation (can be inlined by jit now), split eval function to code and visit

--- funfind.py
+++ funfind.py
@@ -1,8 +1,9 @@
+import argparse
+
 from pyevolve import Util, GTree, GSimpleGA, Consts
 import math
- 
-error_accum = Util.ErrorAccumulator()
- 
+import sys
+
 # This is the functions used by the GP core,
 # Pyevolve will automatically detect them
 # and the they number of arguments
@@ -29,47 +30,64 @@ def eval_node(node, terminals, functions):
         func = functions[node.node_data]
         return func(*args)
 
-def eval_func(chromosome):
-   global error_accum
-   error_accum.reset()
-   #code_com = chromosome.getCompiledCode()
-   root = chromosome.getRoot()
-   for a in xrange(0, 5):
-      for b in xrange(0, 5):
-          a = float(a)
-          b = float(b)
-          # The eval will execute a pre-compiled syntax tree
-          # as a Python expression, and will automatically use
-          # the "a" and "b" variables (the terminals defined)
-          #evaluated     = eval(code_comp, {'a':a, 'b':b}, functions)
-          evaluated     = eval_node(root, {'a':a, 'b':b}, functions)
-          target        = math.sqrt((a*a)+(b*b))
-          error_accum += (target, evaluated)
-   return error_accum.getRMSE()
- 
-def main_run():
-   genome = GTree.GTreeGP()
-   genome.setParams(max_depth=5, method="ramped")
-   genome.evaluator.set(eval_func)
- 
-   ga = GSimpleGA.GSimpleGA(genome)
-   # This method will catch and use every function that
-   # begins with "gp", but you can also add them manually.
-   # The terminals are Python variables, you can use the
-   # ephemeral random consts too, using ephemeral:random.randint(0,2)
-   # for example.
-   ga.setParams(gp_terminals       = ['a', 'b'],
-                gp_function_prefix = "gp")
-   # You can even use a function call as terminal, like "func()"
-   # and Pyevolve will use the result of the call as terminal
-   ga.setMinimax(Consts.minimaxType["minimize"])
-   ga.setGenerations(1000)
-   ga.setMutationRate(0.08)
-   ga.setCrossoverRate(1.0)
-   ga.setPopulationSize(2000)
-   ga.evolve(freq_stats=2)
- 
-   print ga.bestIndividual()
- 
+def eval_nodes(chromosome):
+    error_accum = Util.ErrorAccumulator()
+    root = chromosome.getRoot()
+    for a in xrange(0, 5):
+        for b in xrange(0, 5):
+            a = float(a)
+            b = float(b)
+            evaluated     = eval_node(root, {'a':a, 'b':b}, functions)
+            target        = math.sqrt((a*a)+(b*b))
+            error_accum.append(target, evaluated)
+            return error_accum.getRMSE()
+
+def eval_code(chromosome):
+    error_accum = Util.ErrorAccumulator()
+    code_comp = chromosome.getCompiledCode()
+    root = chromosome.getRoot()
+    for a in xrange(0, 5):
+        for b in xrange(0, 5):
+            a = float(a)
+            b = float(b)
+            # The eval will execute a pre-compiled syntax tree
+            # as a Python expression, and will automatically use
+            # the "a" and "b" variables (the terminals defined)
+            evaluated     = eval(code_comp, {'a':a, 'b':b}, functions)
+            target        = math.sqrt((a*a)+(b*b))
+            error_accum.append(target, evaluated)
+            return error_accum.getRMSE()
+
+
+def main_run(eval_func):
+    genome = GTree.GTreeGP()
+    genome.setParams(max_depth=5, method="ramped")
+    genome.evaluator.set(eval_func)
+
+    ga = GSimpleGA.GSimpleGA(genome)
+    # This method will catch and use every function that
+    # begins with "gp", but you can also add them manually.
+    # The terminals are Python variables, you can use the
+    # ephemeral random consts too, using ephemeral:random.randint(0,2)
+    # for example.
+    ga.setParams(gp_terminals       = ['a', 'b'],
+                 gp_function_prefix = "gp")
+    # You can even use a function call as terminal, like "func()"
+    # and Pyevolve will use the result of the call as terminal
+    ga.setMinimax(Consts.minimaxType["minimize"])
+    ga.setGenerations(500)
+    ga.setMutationRate(0.08)
+    ga.setCrossoverRate(1.0)
+    ga.setPopulationSize(1500)
+    ga.evolve(freq_stats=20)
+
+    #print ga.bestIndividual()
+
 if __name__ == "__main__":
-   main_run()
+
+    if 'code' in sys.argv:
+        eval_func = eval_code
+    else:
+        eval_func = eval_nodes
+
+    main_run(eval_func)
