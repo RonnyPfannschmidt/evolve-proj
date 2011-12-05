f78eb62621698c7130d748048e76ec449c5eaa21
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2011-12-05 15:28:10 +0100

    propper bash TIMEFORMAT for speedtest

--- tools/speedtest.sh
+++ tools/speedtest.sh
@@ -1,12 +1,16 @@
+#!/bin/bash
+
 function pypy-bin() {
     /home/ronny/Projects/pypy/pypy-bin/bin/pypy $@
 }
+
+TIMEFORMAT=$'  real %lE\n  user %lU\n  sys %lS'
 
 for py in pypy-bin python
 do
     for kind in stack visit code
     do
-        echo -n $py $kind
+        echo $py $kind
         time {
             PYTHONPATH=../pyevolve $py funfind.py $kind >/dev/null
         }
